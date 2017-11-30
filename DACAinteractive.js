ed6e39f48d4fceb3dcb8cb979a6052efa3d1c86e
austinlai810 <32529932+austinlai810@users.noreply.github.com>
2017-11-29 18:04:27 -0800

    Add files via upload

--- DACAinteractive.js
+++ DACAinteractive.js
@@ -1,105 +1,122 @@
-var state = 'Today';
-function myfunction(){
-	document.getElementById("button").innerHTML = "Today, the amount of people that will lose DACA is 6% of the content sent everyday on Snapchat.";
-	state = 'Today'
+var state = '2017';
+var myIndex = 0;
+carousel();
+
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("mySlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    myIndex++;
+    if (myIndex > x.length) {
+        myIndex = 1
+    }
+    x[myIndex - 1].style.display = "block";
+    setTimeout(carousel, 3000); // Change image every 2 seconds
 }
 
 
 
 
-function greenfunction(){
-	document.getElementById("button").innerHTML = "In 2018, the number of people who will lose DACA is about equal to the number of teachers employed in California.";
-	state = '2018'
+function seventeenfunction() {
+    document.getElementById("button").innerHTML = "In 2017, the amount of people at risk of losing DACA is close to the amount of minutes in a year.";
+    state = '2017'
 }
 
 
-function bluefunction(){
-	document.getElementById("button").innerHTML = "In 2019, the number of people who will have lost DACA is about the number of Apple computers sold in a month,";
-	state = '2019'
+
+function eightteenfunction() {
+    document.getElementById("button").innerHTML = "In 2018, the number of people who will lose DACA is about equal to the number of teachers employed in California.";
+    state = '2018'
+}
+
+
+
+function nineteenfunction() {
+    document.getElementById("button").innerHTML = "In 2019, the number of people who will have lost DACA is about the number of Apple computers sold in a month.";
+    state = '2019'
 
 }
 
 var scaleValue = 1;
 var canvasMax = 360;
 
+
 function setup() {
-  var cWidth = document.getElementById('canvasContainer').offsetWidth;
-  var myCanvas = createCanvas(min(canvasMax,cWidth), min(canvasMax,cWidth));
-  myCanvas.parent('canvasContainer');
+    var cWidth = document.getElementById('chart').offsetWidth;
+    var myCanvas = createCanvas(min(canvasMax, cWidth), min(canvasMax, cWidth));
+    myCanvas.parent('chart');
 }
 
 
 function draw() {
-  push();
-  scale(scaleValue);
-  background('#ffffff');
-  
+    push();
+    scale(scaleValue);
+    background('#ffffff');
 
-  var countX = 0;
-  var countY = 0;
-  var index = 0;
-  fill("#FCA311");
 
-  while(countX < 10){
-	countY = 0;
-	while(countY < 8){
-		index = index + 1;
-	 	countY = countY + 1;
-	 	if(state == "Today"){
-	 		if(index > 74){
-	 			fill("#81B29A");
-	 		}
-	 	}else if(state == "2017"){
-	 		if(index > 0 && index < 15){
-	 			fill("#FF6A5C");
+    var countX = 0;
+    var countY = 0;
+    var index = 0;
+    var tota2017 = 10;
+    var tota2018 = 10;
+    var tota2019 = 10;
 
-	 		}else if(index >= 15 && index <= 74){
-	 			fill("#FCA311");
-	 		}else if(index > 74){
-	 			fill("#81B29A");
+    fill("#FCA311");
 
-	 		}
-	 	}else if(state == "2018"){
-	 		if(index > 0 && index < 28){
-	 			fill("#FF6A5C");
+    while (countX < 11) {
+        countY = 0;
+        while (countY < 7) {
+            index = index + 1;
+            countY = countY + 1;
+            if (state == "2017") {
+                if (index >10) {
+                    fill("#81B29A");
+                }
+            } else if (state == "2018") {
+                if (index > 0 && index < 15) {
+                    fill("#FF6A5C");
 
-	 		}else if(index >= 28 && index <= 74){
-	 			fill("#FCA311");
-	 		}else if(index > 74){
-	 			fill("#81B29A");
+                } else if (index >= 15 && index <= 74) {
+                    fill("#FCA311");
+                } else if (index > 74) {
+                    fill("#81B29A");
 
-	 		}
-	 	}else if(state == "2019"){
-	 		if(index > 0 && index <= 74){
-	 			fill("#FF6A5C");
-	 		}else if(index > 74){
-	 			fill("#81B29A");
+                }
+            } else if (state == "2019") {
+                if (index > 0 && index < 28) {
+                    fill("#FF6A5C");
 
-	 		}
-	 	}
-		ellipse(countX*30 + 15, countY*30, 25, 25);
-	}
-	countX = countX + 1;
-}
-	
+                } else if (index >= 28 && index <= 74) {
+                    fill("#FCA311");
+                } else if (index > 74) {
+                    fill("#81B29A");
 
-  
-  
+                }
+            }
+            ellipse(countX * 30 + 15, countY * 30, 25, 25);
+        }
+        countX = countX + 1;
+    }
 
-  pop();
+
+
+
+
+    pop();
 }
 
-
-function orangefunction(){
-	document.getElementById("button").innerHTML = "In 2017, the amount of people at risk of losing DACA is close to the amount of minutes in a year.";
-	state = '2017'
+function orangefunction() {
+    document.getElementById("button").innerHTML = "In 2017, the amount of people at risk of losing DACA is close to the amount of minutes in a year.";
+    state = '2017'
 
 }
 
 function windowResized() {
-  //cavas size will be resized in case window size has changed
-  var cWidth = document.getElementById('canvasContainer').offsetWidth;
-  var cHeight = document.getElementById('canvasContainer').offsetHeight;
-  scaleValue = min(canvasMax,cWidth)/canvasMax;
-  resizeCanvas(min(canvasMax,cWidth), min(canvasMax,cWidth));
-}[]
+    //cavas size will be resized in case window size has changed
+    var cWidth = document.getElementById('chart').offsetWidth;
+    var cHeight = document.getElementById('chart').offsetHeight;
+    scaleValue = min(canvasMax, cWidth) / canvasMax;
+    resizeCanvas(min(canvasMax, cWidth), min(canvasMax, cWidth));
+}
